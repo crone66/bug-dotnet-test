e04c45cb19d4c5ed48c6df24545168da9d495bd8
Marcel Croonenbroeck <marcel.croonenbroeck@gmail.com>
2024-02-23 17:35:05 +0100

    Added test project

---
+++ bug-dotnet-test/MyTestClass.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bug_dotnet_test
+{
+    [TestClass]
+    public class MyTestClass
+    {
+        [DataRow((char)8)] //html logger to print an error and results in an empty file
+        [DataRow((char)122)]
+        [TestMethod]
+        public void UselessTestMethod(char key)
+        {
+            Assert.IsTrue(key > 0);
+        }
+    }
+}
